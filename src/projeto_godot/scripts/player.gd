e291df90007e23c35006bf4e9641281cf14e10b5
ginocarlo01 <gino.carlo02@gmail.com>
2020-06-16 13:35:51 -0300

    substituindo src

---
+++ src/projeto_godot/scripts/player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+export var velocity = Vector2(0,100)
+
+export var gravity = 500
+
+const UP = Vector2(0, -1)
+
+var direction 
+
+signal life_scale(sc)
+
+export var life = 300
+
+onready var init_life = life
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = 100 * direction
+
+	move_and_slide(velocity, UP)
+
+func _on_damage_area_damage(damage, node) -> void:
+	life -= damage
+	
+	emit_signal("life_scale", (float(self.life) / float(self.init_life)))
+
+
+
+
+func _on_Area2D_area_entered(area):
+	_on_damage_area_damage(50, $".")
