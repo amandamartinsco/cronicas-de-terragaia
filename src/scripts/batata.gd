106f838e0cd4746a4f408da7d0df07986fa256d4
Cainhu <caioenzo92@outlook.com>
2020-06-17 16:21:11 -0300

    Testando drop
    Testando a mecânica dos items dropados pelos inimigos

---
+++ src/scripts/batata.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite.visible = false
+	get_tree().get_nodes_in_group("enemy")[0].connect("pode_dropar", self, "dropar")
+	get_tree().get_nodes_in_group("enemy")[1].connect("pode_dropar", self, "dropar")
+
+func dropar(pd):
+	print($".".position)
+	$".".position = pd
+	$Sprite.visible = true
+	
+	
