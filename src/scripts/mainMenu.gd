b74f20d877d2a8a05ec7380f67d14841f81bb026
Amanda Martins Xavier <amanda.mx@icloud.com>
2020-06-21 18:43:07 -0300

    HUD
    Partially added HUD

--- src/scripts/mainMenu.gd
+++ src/scripts/mainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_enter"):
+		$pressSpace.set_pressed(true)
 		$themeSong.stop()
 		get_tree().change_scene("res://scenes/enemy.tscn")
